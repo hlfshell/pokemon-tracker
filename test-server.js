e5c257dd4510fdb9127ab7a48e0c042c55f2c277
Keith Chester <kchester@gmail.com>
2016-08-22 06:54:52 -0500

    feature: test server + switching index.html to non electron
    test-server is just so I can launch and edit the HTML/websocket stuff
    without the serial port being required.

--- test-server.js
+++ test-server.js
@@ -7,6 +7,7 @@ const Scanner = require('./Models/Scanner.js');
 let scanner;
 const express = require('express');
 const app = express();
+const path = require('path');
 
 //JSON object of pokemon credentials, with username/password for each
 const pokemonCredentials = require('./pokemon-credentials.json');
@@ -17,24 +18,24 @@ wss.on('connection', function connection(ws){
 });
 
 //Load up and handle all of the gps stuff
-const gps = new GPS(process.argv[2]);
-gps.connect(function(err){
-	if(err){
-		console.log("Something has gone wrong opening the serial port", err);
-		process.exit(0);
-	}
+// const gps = new GPS(process.argv[2]);
+// gps.connect(function(err){
+// 	if(err){
+// 		console.log("Something has gone wrong opening the serial port", err);
+// 		process.exit(0);
+// 	}
 
-	gps.onUpdate = function(){ 
-		mainWindow.webContents.send('gps', gps.location);
-		if(!scanner){
-			scanner = new Scanner();
-			scanner._initTrainers(gps.location);
-			scanner.onScan = onScan;
-		} else {
-			scanner.update(gps.location);
-		}
-	}
-});
+// 	gps.onUpdate = function(){ 
+// 		mainWindow.webContents.send('gps', gps.location);
+// 		if(!scanner){
+// 			scanner = new Scanner();
+// 			scanner._initTrainers(gps.location);
+// 			scanner.onScan = onScan;
+// 		} else {
+// 			scanner.update(gps.location);
+// 		}
+// 	}
+// });
 
 //Function to be called upon scan info updates
 var onScan = function onScan(){
@@ -43,6 +44,6 @@ var onScan = function onScan(){
 
 //Handle the express app setup - very basic web server
 app.use(express.static('Static'));
-app.get('/', (req, res) => res.sendFile("Static/views/index.html"));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, "Static/views/index.html")));
 
 app.listen(9000);
